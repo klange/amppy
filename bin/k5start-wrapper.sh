5b76f2e3ca097165e1d46d325a1e6558141ef111
Kevin Lange <kevin.lange@dakko.us>
2012-06-10 17:38:59 -0700

    A fresh start. The fork is more symbolic than anything else.

--- bin/k5start-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-if [ "$1" = 'player-start' ]; then
-	BGOPT=-b
-	ND=--nodaemonize
-fi
-
-/usr/bin/k5start $BGOPT -t -f /etc/soda.keytab -K 120 tunez -- /usr/bin/perl /afs/acm.uiuc.edu/project/acoustics/bin/acoustics ${1+"$@"} $ND
--- bin/streaming-next-song.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# "stream" below is the name of the Acoustics player ID that we use
-# if you want to change the name, copy this file, change that value
-# and edit your ezstream configuration to point to the new file.
-
-`dirname $0`/acoustics player-song_iterate stream | tail -1
