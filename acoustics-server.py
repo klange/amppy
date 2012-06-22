755bdc1fbc99034958d6add9dda412d9c3df6b74
Julian Krause <julian.krause@gmail.com>
2012-06-21 18:44:23 -0700

    Metaclass magic and removing globals.

--- acoustics-server.py
+++ acoustics-server.py
@@ -12,19 +12,36 @@ import amp.rpc.local
 
 PORT = 6969
 
-class Mode(object):
+mode_registry = {}
+
+
+class ModeMeta(type):
+	def __new__(cls, name, bases, dct):
+		new_cls = super(ModeMeta, cls).__new__(cls, name, bases, dct)
+		if 'name' in dct:
+			mode_registry[dct['name']] = new_cls
+		if 'names' in dct:
+			for name in dct['names']:
+				mode_registry[name] = new_cls
+		return new_cls
+
+
+class Mode(metaclass=ModeMeta):
 	def __init__(self, server, session):
 		self.owner = server
 		self.session = session
-		pass
+
 	def get(self, args):
 		return (400, {"api_error": "Internal API error: You have requested a mode which was initialized by not implemented by the server."})
 
+
 class ModeStatus(Mode):
+	name = "status"
+
 	def get(self, args):
 		output = {}
 		output["selected_player"] = self.session._player
-		output["players"] = self.owner.getPlayers()
+		output["players"] = self.owner.players
 		output["player"] = self.session.player()
 		output["now_playing"] = self.session.currentSong()
 		output["playlist"] = self.owner.db.PlayerQueue(self.session._player)
@@ -33,11 +50,14 @@ class ModeStatus(Mode):
 		output["is_admin"] = self.session.is_admin()
 		return (200, output)
 
+
 class ModeGlobalStatus(Mode):
+	name = "global_status"
+
 	def get(self, args):
 		output = {}
 		output["who"] = self.session.user()
-		output["player_names"] = self.owner.getPlayers()
+		output["player_names"] = self.owner.players
 		output["players"] = {}
 		for i in output['player_names']:
 			x = self.owner.db.SELECT("players", {"player_id": i})
@@ -48,7 +68,10 @@ class ModeGlobalStatus(Mode):
 				output["players"][i] = pl
 		return (200, output)
 
+
 class ModeSearch(Mode):
+	name = "paged_search"
+
 	def get(self, args):
 		if "value" not in args:
 			return (400, {"api_error": "Search requests require a 'value' argument."})
@@ -59,7 +82,10 @@ class ModeSearch(Mode):
 		results = self.owner.db.Search(args["value"], limit, offset)
 		return (200, results)
 
+
 class ModeQuickSearch(Mode):
+	name = "quick_search"
+
 	def get(self, args):
 		if "q" not in args:
 			return (400, {"api_error": "Quick search results a 'q' argument."})
@@ -68,21 +94,30 @@ class ModeQuickSearch(Mode):
 		results = self.owner.db.QuickSearch(args["q"], limit)
 		return (200, results)
 
+
 class ModeRandom(Mode):
+	name = "random"
+
 	def get(self, args):
 		limit = 10
 		if "amount" in args: limit = args["amount"]
 		results = self.owner.db.Random(limit)
 		return (200, results)
 
+
 class ModeRecent(Mode):
+	name = "recent"
+
 	def get(self, args):
 		limit = 10
 		if "amount" in args: limit = args["amount"]
 		results = self.owner.db.Recent(limit)
 		return (200, results)
 
+
 class ModeHistory(Mode):
+	name = "history"
+
 	def get(self, args):
 		limit = 10
 		voter = None
@@ -98,7 +133,10 @@ class ModeHistory(Mode):
 				history.append(song)
 		return (200, results)
 
+
 class ModeDetails(Mode):
+	name = "get_details"
+
 	def get(self, args):
 		if "song_id" not in args:
 			return (400, {"api_error": "get_details requires a 'song_id'"})
@@ -110,7 +148,10 @@ class ModeDetails(Mode):
 			obj["who"].append(i['who'])
 		return (200, {"song": obj})
 
+
 class ModeArt(Mode):
+	name = "art"
+
 	def get(self, args):
 		if "song_id" not in args:
 			return (400, {"api_error": "art requires a 'song_id'"})
@@ -134,7 +175,10 @@ class ModeArt(Mode):
 		f.close()
 		return (200, filecontents, "image/png")
 
+
 class ModeSelect(Mode):
+	name = "select"
+
 	def get(self, args):
 		if "field" not in args:
 			return (400, {"api_error": "select requires a 'field' argument."})
@@ -144,15 +188,20 @@ class ModeSelect(Mode):
 
 
 class ModeChangePlayer(Mode):
+	name = "change_player"
+
 	def get(self, args):
 		if "player_id" not in args:
 			return (400, {"api_error": "Player change requires a player to change to."})
-		if args["player_id"] not in self.owner.getPlayers():
+		if args["player_id"] not in self.owner.players:
 			return (400, {"api_error": "Bad player id."})
 		self.session._player = args["player_id"]
 		return ModeStatus.get(self, args)
 
+
 class ModeTopVoted(Mode):
+	name = "top_voted"
+
 	def get(self, args):
 		if "limit" in args:
 			limit = args["limit"]
@@ -160,13 +209,19 @@ class ModeTopVoted(Mode):
 			limit = 10
 		return (200, self.owner.db.TopVoted(limit))
 
+
 class ModeAlbumSearch(Mode):
+	name = "album_search"
+
 	def get(self, args):
 		if "album" not in args:
 			return (400, {"api_error": "album_search requires an 'album' argument."})
 		return (200, self.owner.db.AlbumSearch(args["album"]))
 
+
 class ModeVote(Mode):
+	name = "vote"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to vote for songs."})
@@ -181,7 +236,10 @@ class ModeVote(Mode):
 			self.owner.db.AddVote(self.session.user(), self.session._player, args['song_id'], priorityNumber)
 		return ModeStatus.get(self, args)
 
+
 class ModeUnvote(Mode):
+	name = "unvote"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to vote for songs."})
@@ -194,7 +252,10 @@ class ModeUnvote(Mode):
 			self.owner.db.Unvote(self.session.user(), args['song_id'])
 		return ModeStatus.get(self, args)
 
+
 class ModePlaylists(Mode):
+	name = "playlists"
+
 	def get(self, args):
 		who = ""
 		title = ""
@@ -202,35 +263,47 @@ class ModePlaylists(Mode):
 		if "title" in args: title = args["title"]
 		return (200, self.owner.db.Playlists(who,title))
 
+
 class ModePlaylistsLoose(Mode):
+	name = "playlists_loose"
+
 	def get(self, args):
 		value = ""
 		if "value" in args: value = args["value"]
 		return (200, self.owner.db.PlaylistsLoose(value))
 
+
 class ModePlaylistContents(Mode):
+	name = "playlist_contents"
+
 	def get(self, args):
 		if not "playlist_id" in args:
 			return (400, {"api_error": "playlist_contents requires a 'playlist_id' argument."})
 		return (200, self.owner.db.PlaylistContents(args["playlist_id"]))
 
+
 class ModePlaylistInfo(Mode):
+	name = "playlist_info"
+
 	def get(self, args):
 		if not "playlist_id" in args:
 			return (400, {"api_error": "playlist_info requires a 'playlist_id' argument."})
 		return (200, self.owner.db.PlaylistInfo(args["playlist_id"]))
 
+
 class ModeAddToPlaylist(Mode):
+	name = "add_to_playlist"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to modify playlists."})
 		if "song_id" not in args:
 			return (400, {"api_error": "add_to_playlist requires a 'song_id' argument."})
 		if not "playlist_id" in args:
-			return (400, {"api_error", "add_to_playlist requires a 'playlist_id' argument."})
+			return (400, {"api_error": "add_to_playlist requires a 'playlist_id' argument."})
 		(discard, playlist) = ModePlaylistInfo.get(self, args)
 		if playlist['who'] != self.session.user() and not self.session.is_admin():
-			return (500, {"auth_error", "You are not permitted to modify this playlist."})
+			return (500, {"auth_error": "You are not permitted to modify this playlist."})
 		if ";" in args["song_id"]:
 			for i in args["song_id"].split(";"):
 				self.owner.db.AddToPlaylist(args["playlist_id"], i)
@@ -238,17 +311,19 @@ class ModeAddToPlaylist(Mode):
 			self.owner.db.AddToPlaylist(args["playlist_id"], args['song_id'])
 		return ModePlaylistContents.get(self, args)
 
+
 class ModeRemoveFromPlaylist(Mode):
+	name = "remove_from_playlist"
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to modify playlists."})
 		if "song_id" not in args:
 			return (400, {"api_error": "remove_from_playlist requires a 'song_id' argument."})
 		if not "playlist_id" in args:
-			return (400, {"api_error", "remove_from_playlist requires a 'playlist_id' argument."})
+			return (400, {"api_error": "remove_from_playlist requires a 'playlist_id' argument."})
 		(discard, playlist) = ModePlaylistInfo.get(self, args)
 		if playlist['who'] != self.session.user() and not self.session.is_admin():
-			return (500, {"auth_error", "You are not permitted to modify this playlist."})
+			return (500, {"auth_error": "You are not permitted to modify this playlist."})
 		if ";" in args["song_id"]:
 			for i in args["song_id"].split(";"):
 				self.owner.db.RemoveFromPlaylist(args["playlist_id"], i)
@@ -256,30 +331,39 @@ class ModeRemoveFromPlaylist(Mode):
 			self.owner.db.RemoveFromPlaylist(args["playlist_id"], args['song_id'])
 		return ModePlaylistContents.get(self, args)
 
+
 class ModeCreatePlaylist(Mode):
+	name = "create_playlist"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to modify playlists."})
 		if not "title" in args:
-			return (400, {"api_error", "create_playlist requires a 'title' argument."})
+			return (400, {"api_error": "create_playlist requires a 'title' argument."})
 		self.owner.db.CreatePlaylist(self.session.user(), args["title"])
 		args["who"] = self.session.user()
 		return ModePlaylists.get(self, args)
 
+
 class ModeDeletePlaylist(Mode):
+	name = "delete_playlist"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to modify playlists."})
 		if not "playlist_id" in args:
-			return (400, {"api_error", "delete_playlist requires a 'playlist_id' argument."})
+			return (400, {"api_error": "delete_playlist requires a 'playlist_id' argument."})
 		(discard, playlist) = ModePlaylistInfo.get(self, args)
 		if playlist['who'] != self.session.user() and not self.session.is_admin():
-			return (500, {"auth_error", "You are not permitted to modify this playlist."})
+			return (500, {"auth_error": "You are not permitted to modify this playlist."})
 		self.owner.db.DeletePlaylist(args["playlist_id"])
 		args["who"] = self.session.user()
 		return ModePlaylists.get(self, args)
 
+
 class ModePurge(Mode):
+	name = "purge"
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "This action can only be executed by logged-in uesrs."})
@@ -290,6 +374,8 @@ class ModePurge(Mode):
 
 
 class ModeStats(Mode):
+	name = "stats"
+
 	def get(self, args):
 		who = None
 		if "who" in args: who = args["who"]
@@ -298,7 +384,10 @@ class ModeStats(Mode):
 		output["top_artists"] = self.owner.db.TopArtists(who)
 		return (200, output)
 
+
 class ModeControls(Mode):
+	names = ["start", "stop", "skip", "pause", "volume", "zap"]
+
 	def get(self, args):
 		if not self.session.user():
 			return (500, {"auth_error": "You must login to control the player."})
@@ -315,21 +404,26 @@ class AcousticsSession(object):
 		self.owner   = owner
 		self.sessid  = sessid
 		self._user   = None
-		self._player = self.owner.getPlayers()[0]
+		self._player = self.owner.players[0]
 		self.created = int(time.time())
+
 	def is_admin(self):
 		return True
+
 	def can_skip(self):
 		# Should be current_song->who = me || current_song->who = None
 		return True
+
 	def user(self):
 		return self._user
+
 	def player(self):
 		x = self.owner.db.SELECT("players", {"player_id": self._player})
 		if x:
 			return x[0]
 		else:
 			return None
+
 	def currentSong(self):
 		if not self.player():
 			return None
@@ -341,88 +435,58 @@ class AcousticsSession(object):
 			obj["who"].append(i['who'])
 		return obj
 
+
 class AcousticsServer(object):
 	def __init__(self):
-		self.modes = {}
+		self.modes = mode_registry
 		self.config = config.AcousticsConfig()
 		self.db = db.Sqlite(self.config["database"]["data_source"].split(":")[-1])
 		self.sessions = {}
 		self.players = self.config["{}"]['players'].split(",")
-	def addMode(self, name, mode_type):
-		self.modes[name] = mode_type
-	def getPlayers(self):
-		return self.players
+
 	def newSession(self):
 		sid = str(uuid.uuid1())
 		self.sessions[sid] = AcousticsSession(sid, self)
 		return sid
+
 	def rpc(self, player_id, args):
 		if player_id in self.players:
 			rpc_module = importlib.import_module(self.config.translate(self.config['player.'+player_id]["rpc"]))
-			print(rpc_module)
 			player = rpc_module.RPC()
 			player.execute(player_id, args)
+
 	def execute(self, session, args):
 		if args["mode"] in self.modes:
-			return self.modes[args["mode"]](server, self.sessions[session]).get(args)
+			return self.modes[args["mode"]](self, self.sessions[session]).get(args)
 		else:
 			return (400, {"api_error": "Unrecognized mode `%s`" % args["mode"]})
 
-server = AcousticsServer()
-server.addMode("status", ModeStatus)
-server.addMode("global_status", ModeGlobalStatus)
-server.addMode("paged_search", ModeSearch)
-server.addMode("quick_search", ModeQuickSearch)
-server.addMode("random", ModeRandom)
-server.addMode("history", ModeHistory)
-server.addMode("recent", ModeRecent)
-server.addMode("change_player", ModeChangePlayer)
-server.addMode("get_details", ModeDetails)
-server.addMode("art", ModeArt)
-server.addMode("top_voted", ModeTopVoted)
-server.addMode("album_search", ModeAlbumSearch)
-server.addMode("select", ModeSelect)
-server.addMode("vote", ModeVote)
-server.addMode("unvote", ModeUnvote)
-server.addMode("stats", ModeStats)
-server.addMode("playlists", ModePlaylists)
-server.addMode("playlists_loose", ModePlaylistsLoose)
-server.addMode("playlist_contents", ModePlaylistContents)
-server.addMode("playlist_info", ModePlaylistInfo)
-server.addMode("add_to_playlist", ModeAddToPlaylist)
-server.addMode("remove_from_playlist", ModeRemoveFromPlaylist)
-server.addMode("create_playlist", ModeCreatePlaylist)
-server.addMode("delete_playlist", ModeDeletePlaylist)
-server.addMode("purge", ModePurge)
 
-# Commands
-server.addMode("start",  ModeControls)
-server.addMode("stop",   ModeControls)
-server.addMode("skip",   ModeControls)
-server.addMode("pause",  ModeControls)
-server.addMode("volume", ModeControls)
-server.addMode("zap",    ModeControls)
+def AcousticsHandlerFactory(server):
+	failures = {}
+	handler = AcousticsHandler
+	handler.acoustics_server = server
+	handler.failures = failures
+	return handler
 
-failures = {}
 
 class AcousticsHandler(http.server.SimpleHTTPRequestHandler):
 	def do_GET(self):
-		global server, failures
 		if 'cookie' in self.headers:
 			self.cookie = http.cookies.SimpleCookie(self.headers["cookie"])
 		else:
 			self.cookie = http.cookies.SimpleCookie()
 		session = None
-		if "sessid" in self.cookie and self.cookie['sessid'].value in server.sessions:
+		if "sessid" in self.cookie and self.cookie['sessid'].value in self.acoustics_server.sessions:
 			session = self.cookie["sessid"].value
 		else:
-			if self.client_address[0] not in failures.keys():
+			if self.client_address[0] not in self.failures.keys():
 				print("First time, trying to give out session key for " + self.client_address[0])
-				failures[self.client_address[0]] = 0
-			failures[self.client_address[0]] += 1
-			session = server.newSession()
+				self.failures[self.client_address[0]] = 0
+			self.failures[self.client_address[0]] += 1
+			session = self.acoustics_server.newSession()
 			self.cookie["sessid"] = session
-			if failures[self.client_address[0]] > 3:
+			if self.failures[self.client_address[0]] > 3:
 				self.send_response(400)
 				self.send_header('Content-type', 'text/html')
 				self.send_header('Set-Cookie', self.cookie)
@@ -441,7 +505,7 @@ class AcousticsHandler(http.server.SimpleHTTPRequestHandler):
 				if self.headers["Authorization"].startswith('Basic '):
 					tmp = base64.standard_b64decode(bytes(self.headers['Authorization'].split(" ")[1].encode('utf-8'))).decode("utf-8")
 					# TODO: Actual authentication
-					server.sessions[session]._user = tmp.split(":")[0]
+					self.acoustics_server.sessions[session]._user = tmp.split(":")[0]
 					self.send_response(200)
 					self.send_header('Location', '/json.pl?mode=status')
 					self.end_headers()
@@ -457,7 +521,7 @@ class AcousticsHandler(http.server.SimpleHTTPRequestHandler):
 				args[k] = ";".join(args[k])
 			if "mode" not in args:
 				args["mode"] = "status"
-			result = server.execute(session, args)
+			result = self.acoustics_server.execute(session, args)
 			if len(result) == 2:
 				(status, results) = result
 				output = json.dumps(results).encode("utf-8")
@@ -473,12 +537,13 @@ class AcousticsHandler(http.server.SimpleHTTPRequestHandler):
 			self.end_headers()
 			self.wfile.write(output)
 		else:
+			self.path = "/web%s" % self.path
 			return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 class AcousticsSocket(socketserver.TCPServer):
 	allow_reuse_address = True
 
 if __name__ == "__main__":
-	os.chdir("web")
-	httpd = AcousticsSocket(("",6969), AcousticsHandler)
+	server = AcousticsServer()
+	httpd = AcousticsSocket(("", PORT), AcousticsHandlerFactory(server))
 	httpd.serve_forever()
